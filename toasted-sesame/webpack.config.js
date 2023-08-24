2da4ae9699166d5a2e5c8389223df129d2f794b0
zzeroyzz <zzeroyzz@gmail.com>
2023-08-24 19:10:20 -0400

    feat: created build file

--- toasted-sesame/webpack.config.js
+++ toasted-sesame/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js', // Replace with your entry file
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'dist'), // Replace with your output directory
+    path: path.resolve(__dirname, 'build'), // Replace with your output directory
     filename: 'bundle.js',
   },
   module: {
